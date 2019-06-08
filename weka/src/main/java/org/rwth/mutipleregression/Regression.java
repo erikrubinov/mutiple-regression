ce4978895987cd601095159be1c547983ae99eef
Alexander Rubinov <alexander.rubinov@7p-group.com>
2019-06-08 22:47:41 +0200

    addiditonal models

--- weka/src/main/java/org/rwth/mutipleregression/Regression.java
+++ weka/src/main/java/org/rwth/mutipleregression/Regression.java
@@ -1,10 +1,11 @@
 package org.rwth.mutipleregression;
 
-import org.springframework.core.io.ClassPathResource;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 
 import weka.classifiers.Evaluation;
 import weka.classifiers.functions.LinearRegression;
-import weka.classifiers.functions.SMOreg;
 import weka.core.Instances;
 import weka.core.converters.ConverterUtils.DataSource;
 /**
@@ -14,14 +15,26 @@ import weka.core.converters.ConverterUtils.DataSource;
 public class Regression {
 	
 	public static void main(String[] args) throws Exception{
-		DataSource source = new DataSource(new ClassPathResource("house.arff",Regression.class).getInputStream());
-		Instances dataset = source.getDataSet();
-		dataset.setClassIndex(dataset.numAttributes()-1);
+		
+		Instances dataset = createDataSet(new File(args[0]));
 		/**
 		 * linear regression model
 		 */
-		LinearRegression lr = new LinearRegression();
-		lr.buildClassifier(dataset);
+		
+		// warm up
+		for(int i = 0; i<100; i++) {
+			calculate(dataset);
+		}
+		final int times = 1000;
+		LinearRegression lr = null;
+		long start = System.currentTimeMillis();
+		for(int i = 0; i<times; i++) {
+			lr = calculate(dataset);
+		}
+		long end = System.currentTimeMillis();
+		
+		System.out.println(String.format("Calculation time: %f ms.", (end - start)/(float)times));
+		
 		System.out.println(lr);
 		Evaluation lreval = new Evaluation(dataset);
 		lreval.evaluateModel(lr, dataset);
@@ -29,11 +42,24 @@ public class Regression {
 		/**
 		 * svm regression model
 		 */
-		SMOreg smoreg = new SMOreg();
-		smoreg.buildClassifier(dataset);
-		Evaluation svmregeval = new Evaluation(dataset);
-		svmregeval.evaluateModel(smoreg, dataset);
-		System.out.println(svmregeval.toSummaryString());
+//		SMOreg smoreg = new SMOreg();
+//		smoreg.buildClassifier(dataset);
+//		Evaluation svmregeval = new Evaluation(dataset);
+//		svmregeval.evaluateModel(smoreg, dataset);
+//		System.out.println(svmregeval.toSummaryString());
 		
+	}
+
+	private static LinearRegression calculate(Instances dataset) throws Exception {
+		LinearRegression lr = new LinearRegression();
+		lr.buildClassifier(dataset);
+		return lr;
+	}
+
+	private static Instances createDataSet(File afrrFile) throws FileNotFoundException, Exception {
+		DataSource source = new DataSource(new FileInputStream(afrrFile));
+		Instances dataset = source.getDataSet();
+		dataset.setClassIndex(dataset.numAttributes()-1);
+		return dataset;
 	}
 }
